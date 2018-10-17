b862566ae219c6c79b315f80198d99a9bedf87eb
stephentetley <stephen.tetley@gmail.com>
2018-10-17 16:16:45 +0100

    Some work on Prolog screens queries.

--- prolog/screens/facts/fact_db.pl
+++ prolog/screens/facts/fact_db.pl
@@ -3,10 +3,14 @@
 % For the time being, this is a useful way of decoupling ultrasonics.pl 
 % from the generated facts database.
 
-:- module(fact_db, []).
-:- reexport([ facts/screens
+:- module(fact_db, 
+            [ screen_type/2
             ]).
+        
+:- reexport([ facts/screens
+            , facts/installations ]).
 
 
 % TODO - should we define accessors for facts with large arities?
 
+screen_type(screen(_, _, _, _, _, Type, _, _), Type).
--- prolog/screens/queries.pl
+++ prolog/screens/queries.pl
@@ -11,10 +11,37 @@
 % (format/2 seems to write to terminal)
 screen_aux(Type) :- 
     screen_category(Type, Cat),
-    !,
-    format("~w ~w\n", [Type, Cat]).
+    format("~w ~w\n", [Type, Cat]), 
+    !.
 
 demo01 :- 
-    findall(X, screen(_, _, _, _, _, X, _, _), List),
+    findall(X, (X = screen(_,_,_,_,_,_,_,_), X), List),
+    maplist(screen_type, List, Types),
+    list_to_ord_set(Types, Xs),
+    maplist(screen_aux, Xs).
+
+
+demo02 :- 
+    findall(Type, screen(_, _, _, _, _, Type, _, _), List),
     list_to_ord_set(List, Xs),
     maplist(screen_aux, Xs).
+
+
+
+site(Name) :- installation(_, Name, _, _).
+site_uid(Uid) :- installation(Uid, _, _, _).
+
+get_kids(Uid, Kids) :- installation(Uid, _, _, kids(Kids)).
+
+
+pg_case(process_group(_,_)) :- true.
+
+pg_name(process_group(Name,_), Name).
+
+% Toplevel...
+process_group_names(Uid, Gs) :- 
+    get_kids(Uid,Kids),
+    convlist([X,Y] >> pg_name(X,Y), Kids, Gs).
+
+
+
