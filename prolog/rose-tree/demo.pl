9df4d24d08bd61c114bc96e774bca8c60727c83e
stephentetley <stephen.tetley@gmail.com>
2019-03-11 10:41:05 +0000

    Digression to difference lists for accumulators, possibly they will cause arity problems for the traversals.

--- prolog/rose-tree/demo.pl
+++ prolog/rose-tree/demo.pl
@@ -127,11 +127,17 @@ demo10(Ans) :-
     tree1(Body),
     onetd_transform(count_trafo, Body, 0, Ans).
 
-accumulate(X, Acc, Ans) :- 
+%% Slow
+snoc(X, Acc, Ans) :- 
     append(Acc, [X], Ans).
 
+/*
 cons(X,Xs,Ans) :- 
     Ans = [X|Xs].
+*/
+
+cons(A,[],[A]) :- !.
+cons(A,[H|T],[A,H|T]).
 
 
 %% Discrepancy - should the "payload" (here the integer 1) appear in the list of children?
@@ -140,7 +146,7 @@ cons(X,Xs,Ans) :-
 %% KURE JFP Paper Section 4.3.2 Alternative Universes
 
 all_children(Input,Ans) :-
-    all_transform(accumulate,Input,[],Ans).
+    all_transform(snoc,Input,[],Ans).
 
 
 demo11(Ans) :-
@@ -161,3 +167,65 @@ child_of_type(Constructor, Input, Ans) :-
 demo12(Ans) :-
     tree1(Body),
     child_of_type(rose_tree, Body, Ans).
+
+%% Digression - diff lists seem to upset the arity of our traversals...
+
+%% diff lists
+dappend(A-B, B-C, A-C).
+
+dl([], L-L).
+dl([H|T], [H|L1]-L2) :-
+    dl(T, L1-L2).
+
+undl(X-[],X).
+
+demo_dl(Ans) :- 
+    dl([1,2,3], Dxs),
+    dl([4,5,6], Dys),
+    dappend(Dxs, Dys, A1),
+    undl(A1,Ans).
+
+/* snocdl(A-B,):-
+    dappend(A-B,) */
+
+%% diff lists without pairs
+dappend(A, B, B, C, A, C).
+
+dl([], L, L).
+dl([H|T], [H|L1], L2) :-
+    dl(T, L1, L2).
+
+dempty(X1, X2) :-
+    dl([],X1,X2).
+
+dsingleton(A, X1, X2) :-
+    dl([A], X1, X2).
+
+%% tail variable remains the same
+dcons(A, [H|T], X2, Y1, X2) :-
+    Y1 = [A,H|T].
+    
+dsnoc(A, X1, X2, Y1, Y2) :-
+    dsingleton(A,B1,B2),
+    dappend(X1,X2,B1,B2,Y1,Y2).
+
+
+
+
+
+undlist(X,[],X).
+
+
+demo_dl2(Ans) :- 
+    dl([1,2,3], X1, X2),
+    dl([4,5,6], Y1, Y2),
+    dappend(X1, X2, Y1, Y2, A1, A2),
+    undlist(A1, A2, Ans).
+
+
+
+
+
+
+    
+
